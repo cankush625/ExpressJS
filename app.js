d3ec7e83e167e8bf7788eed005e7c5e305b26ae8
cankush625 <cankush625@gmail.com>
2022-04-15 12:11:44 +0530

    feat(Route and Query params): Using route params and query params in ExpressJS
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT | 5000;
+
+const products = {"items": [{"id": "1", "name": "Mac Pro"}, {"id": "2", "name": "iPhone"}]};
 
 app.get("/", (req, res) => {
   console.log("home page");
   res.status(200).send("<h1>Home page</h1>");
 });
 
-app.get("/about", (req, res) => {
+app.get("/api/v1/about", (req, res) => {
   console.log("about page");
   res.status(200).send("<h1>About page</h1>");
+});
+
+// Query params
+app.get("/api/v1/products", (req, res) => {
+  const productID = req.query.product_id;
+  if (!productID) {
+    return res.status(200).json(products);
+  }
+  const product = products.items.find((product,) => {return product.id === productID});
+  if (!product) {
+    return res.status(404).send(`Product with ID ${productID} is not found`);
+  }
+  return res.status(200).json(product);
+});
+
+// Route params
+app.get("/api/v1/products/:productID", (req, res) => {
+  const productID = req.params.productID;
+  const product = products.items.find((product,) => {return product.id === productID});
+  if (!product) {
+    return res.status(404).send(`Product with ID ${productID} is not found`);
+  }
+  return res.status(200).json(product);
 });
 
 // Resource not exist
